e407a0a5a7a7d346fa67dd84c16d932832e62800
jiachenning <jiachenning@gmail.com>
2014-04-22 14:50:13 +0800

    finish proguard mixed

--- src/me/ninjachen/liulishuodemo/BitMapRotateActivity.java
+++ src/me/ninjachen/liulishuodemo/BitMapRotateActivity.java
@@ -10,13 +10,15 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Matrix;
-import android.graphics.PointF;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
 
 public class BitMapRotateActivity extends Activity {
 
@@ -26,7 +28,9 @@ public class BitMapRotateActivity extends Activity {
 	Bitmap bm1;
 	Bitmap bm2;
 
-	String urls[] = { "http://llss.qiniudn.com/forum/image/525d1960c008906923000001_1397820588.jpg", "http://llss.qiniudn.com/forum/image/e8275adbeedc48fe9c13cd0efacbabdd_1397877461243.jpg" };
+	String urls[] = {
+			"http://llss.qiniudn.com/forum/image/525d1960c008906923000001_1397820588.jpg",
+			"http://llss.qiniudn.com/forum/image/e8275adbeedc48fe9c13cd0efacbabdd_1397877461243.jpg" };
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -54,9 +58,9 @@ public class BitMapRotateActivity extends Activity {
 				iv1.setImageBitmap(rotate(bm1, 90));
 			}
 		});
-		
+
 		iv1.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
@@ -64,14 +68,44 @@ public class BitMapRotateActivity extends Activity {
 				iv1.setImageBitmap(bm1);
 			}
 		});
-		
+
 		iv2.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				bm2 = rotate(bm2, 90);
-				iv2.setImageBitmap(bm2);
+				Bitmap conBinedBitmap = combineBitmap(bm1, bm2);
+				conBinedBitmap = rotate(conBinedBitmap, 90);
+				 final DisplayMetrics displayMetrics = new DisplayMetrics();
+			        BitMapRotateActivity.this.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+			        final int newHeight = displayMetrics.heightPixels;
+			        final int newWidth = displayMetrics.widthPixels;
+			        
+			        float scaleWidth = ((float) newWidth) / conBinedBitmap.getWidth();
+				    float scaleHeight = ((float) newHeight) / conBinedBitmap.getHeight();
+				    // 取得想要缩放的matrix参数
+				    Matrix matrix = new Matrix();
+				    matrix.postScale(scaleWidth, scaleHeight);
+				    // 得到新的图片
+				    Bitmap fullbm = Bitmap.createBitmap(conBinedBitmap, 0, 0, conBinedBitmap.getWidth(), conBinedBitmap.getHeight(), matrix,
+				      true);
+//				    BitMapRotateActivity.this.requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
+				    fullbm = rotate(fullbm, 90);
+				    iv1.setScaleType(ScaleType.FIT_XY);
+				    iv1.setImageBitmap(fullbm);
+				    iv2.setVisibility(View.GONE);
+				    rotate.setVisibility(View.GONE);
+				    
+				    iv1.setOnClickListener(null);
+				    iv2.setOnClickListener(null);
+				// Intent i = new Intent(BitMapRotateActivity.this,
+				// ImageDetailActivity.class);
+				// Bundle extras = new Bundle();
+				// // extras.p
+				// i.putExtras(extras);
+				// startActivity(i);
+				// bm2 = rotate(bm2, 90);
+				// iv2.setImageBitmap(bm2);
 			}
 		});
 	}
@@ -91,48 +125,42 @@ public class BitMapRotateActivity extends Activity {
 	}
 
 	/**
-	 * 将多个Bitmap合并成一个图片。
+	 * 合并两张bitmap为一张
 	 * 
-	 * @param int 将多个图合成多少列
-	 * @param Bitmap
-	 *            ... 要合成的图片
-	 * @return
+	 * @param firstBitmap
+	 * @param secondBitmap
+	 * @return Bitmap
 	 */
-//	public static Bitmap combineBitmaps(int columns, Bitmap... bitmaps) {
-//		if (columns <= 0 || bitmaps == null || bitmaps.length == 0) {
-//			throw new IllegalArgumentException(
-//					"Wrong parameters: columns must > 0 and bitmaps.length must > 0.");
-//		}
-//		int maxWidthPerImage = 0;
-//		int maxHeightPerImage = 0;
-//		for (Bitmap b : bitmaps) {
-//			maxWidthPerImage = maxWidthPerImage > b.getWidth() ? maxWidthPerImage
-//					: b.getWidth();
-//			maxHeightPerImage = maxHeightPerImage > b.getHeight() ? maxHeightPerImage
-//					: b.getHeight();
-//		}
-//		int rows = 0;
-//		if (columns >= bitmaps.length) {
-//			rows = 1;
-//			columns = bitmaps.length;
-//		} else {
-//			rows = bitmaps.length % columns == 0 ? bitmaps.length / columns
-//					: bitmaps.length / columns + 1;
-//		}
-//		Bitmap newBitmap = Bitmap.createBitmap(columns * maxWidthPerImage, rows
-//				* maxHeightPerImage, Config.RGB_565);
-//
-//		for (int x = 0; x < rows; x++) {
-//			for (int y = 0; y < columns; y++) {
-//				int index = x * columns + y;
-//				if (index >= bitmaps.length)
-//					break;
-//				newBitmap = mixtureBitmap(newBitmap, bitmaps[index],
-//						new PointF(y * maxWidthPerImage, x * maxHeightPerImage));
-//			}
-//		}
-//		return newBitmap;
-//	}
+	public static Bitmap combineBitmap(Bitmap firstBitmap, Bitmap secondBitmap) {
+		if (firstBitmap == null) {
+			return null;
+		}
+		int bgWidth = firstBitmap.getWidth();
+		int bgHeight = firstBitmap.getHeight();
+		int fgWidth = secondBitmap.getWidth();
+		int fgHeight = secondBitmap.getHeight();
+		
+		float scaleWidth = ((float) bgWidth) / fgWidth;
+//	    float scaleHeight = scaleWidth;
+	    // 取得想要缩放的matrix参数
+	    Matrix matrix = new Matrix();
+	    matrix.postScale(scaleWidth, 1);
+	    // 得到新的图片
+	    Bitmap newbm = Bitmap.createBitmap(secondBitmap, 0, 0, fgWidth, fgHeight, matrix,
+	      true);
+	    
+		int maxWidth = bgWidth ;
+//		int maxWidth = bgWidth > fgWidth ? bgWidth : fgWidth;
+		Bitmap newmap = Bitmap
+				.createBitmap(maxWidth, bgHeight + fgHeight, Config.ARGB_8888);
+		Canvas canvas = new Canvas(newmap);
+		canvas.drawBitmap(firstBitmap, 0, 0, null);
+		canvas.drawBitmap(newbm, 0,
+				bgHeight, null);
+		canvas.save(Canvas.ALL_SAVE_FLAG);
+		canvas.restore();
+		return newmap;
+	}
 
 	// 旋转图片
 	// Rotates the bitmap by the specified degree.
@@ -145,7 +173,7 @@ public class BitMapRotateActivity extends Activity {
 			try {
 				b = Bitmap.createBitmap(b, 0, 0, b.getWidth(), b.getHeight(),
 						m, true);
-				
+
 			} catch (OutOfMemoryError ex) {
 				// We have no memory to rotate. Return the original bitmap.
 			}
