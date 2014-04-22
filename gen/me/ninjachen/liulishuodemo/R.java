b9bcecc71150b4438afd9c4560931e8fe41b66b2
jiachenning <jiachenning@gmail.com>
2014-04-22 10:13:11 +0800

    no message

--- gen/me/ninjachen/liulishuodemo/R.java
+++ gen/me/ninjachen/liulishuodemo/R.java
@@ -1094,14 +1094,15 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050041;
+        public static final int action_settings=0x7f050043;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
-        public static final int btn_anim=0x7f05003d;
-        public static final int btn_network=0x7f05003f;
-        public static final int btn_recorder=0x7f05003e;
-        public static final int btn_translate=0x7f050040;
+        public static final int btn_anim=0x7f05003e;
+        public static final int btn_network=0x7f050040;
+        public static final int btn_play=0x7f050042;
+        public static final int btn_recorder=0x7f05003f;
+        public static final int btn_translate=0x7f050041;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
@@ -1120,6 +1121,7 @@ containing a value of this type.
         public static final int iv_main=0x7f05003c;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
+        public static final int listview=0x7f05003d;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
@@ -1201,8 +1203,10 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_anim=0x7f030018;
-        public static final int activity_main=0x7f030019;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
+        public static final int activity_list=0x7f030019;
+        public static final int activity_main=0x7f03001a;
+        public static final int activity_recorder=0x7f03001b;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
@@ -1252,9 +1256,10 @@ containing a value of this type.
         public static final int anim=0x7f0a0010;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
-        public static final int network=0x7f0a0012;
+        public static final int network=0x7f0a0013;
+        public static final int play=0x7f0a0012;
         public static final int recorder=0x7f0a0011;
-        public static final int translate=0x7f0a0013;
+        public static final int translate=0x7f0a0014;
     }
     public static final class style {
         /** 
---
+++ src/me/ninjachen/liulishuodemo/RecorderActivity.java
@@ -0,0 +1,15 @@
+package me.ninjachen.liulishuodemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RecorderActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_recorder);
+		
+	}
+}
